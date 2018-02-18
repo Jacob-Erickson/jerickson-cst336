1773e2ca1ebb55b4b413f4d8016849b8a2492ba3
Jacob Erickson <jerickson@csumb.edu>
2018-02-18 02:35:34 +0000

    Final Version of Lab 2

--- Labs/Lab_2/777/inc/functions.php
+++ Labs/Lab_2/777/inc/functions.php
@@ -4,7 +4,7 @@ function play()
 {
     
     for ($i=1; $i<4; $i++){
-        ${"randomValue" . $i } = 0/*rand(0,3)*/;
+        ${"randomValue" . $i } = rand(0,3);
         displaySymbol(${"randomValue" . $i }, $i);
     }
     
