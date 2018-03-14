7965d689fc0700f8cc264e392fd098332ec548dc
Jacob Erickson <jerickson@csumb.edu>
2018-03-14 21:14:14 +0000

    Modified Navigation

--- Homework/homework_index.php
+++ Homework/homework_index.php
@@ -9,6 +9,7 @@
         <nav>
             <a href="Homework_1/index.html">Homework #1</a>
             <a href="Homework_2/index.html">Homework #2</a>
+            <a href="Homework_3/index.php">Homework #3</a>
         </nav>
     </body>
 </html>
