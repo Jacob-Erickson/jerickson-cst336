8ac533c876fed9e82262905fc388f06b9b3ae88a
Jacob Erickson <jerickson@csumb.edu>
2018-04-11 23:16:19 +0000

    fixed commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Jacob Erickson's Repository </title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        
+        <h1>Assignment Directory</h1>
+        
+        <table>
+            <tr>
+                <td><a href="Challenges/challenges_index.php">Challenges</a></td>
+                <td><a href="Homework/homework_index.php">Homework</a></td>
+                <td><a href="Labs/lab_index.php">Labs</a></td>
+                <td><a href="Practice/randomColor.php">Practice</a></td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="/team_project/index.php">Team Project</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Final Project
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
