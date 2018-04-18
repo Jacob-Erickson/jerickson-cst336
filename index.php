cd2e7075e8409ef79715738a1f22aa3a294e3cf3
Jacob Erickson <jerickson@csumb.edu>
2018-04-18 21:11:36 +0000

    fixed heroku navigation

--- index.php
+++ index.php
@@ -18,34 +18,34 @@
                 <td><strong>Projects</strong></td>
             </tr>
             <tr>
-                <td><a href="Lab_1/PersonalPortfolio/index.html">Lab #1: <br /> Personal Portfolio </a></td>
-                <td><a href="Homework_1/index.html">Homework #1: <br /> Computer Science Subject</a></td>
-                <td><a href="Challenge_1/seasons_home.html">Challenge #1: <br /> Seasonal Images</a></td>
+                <td><a href="Labs/Lab_1/PersonalPortfolio/index.html">Lab #1: <br /> Personal Portfolio </a></td>
+                <td><a href="Homework/Homework_1/index.html">Homework #1: <br /> Computer Science Subject</a></td>
+                <td><a href="Challenges/Challenge_1/seasons_home.html">Challenge #1: <br /> Seasonal Images</a></td>
                 <td><a href="Practice/randomColor.php">Practice #1: <br /> Random Color Generator</a></td>
                 <td><a href="/CST336-Team_Project/index.php">Team Project</a></td>
             </tr>
             <tr>
-                <td><a href="Lab_2/777/index.php">Lab #2: <br /> Slot Machine Game </a></td>
-                <td><a href="Homework_2/index.html">Homework #2: <br /> Vocab Trainer</a></td>
-                <td><a href="Challenge_2/Version_2/card_game_v2.php">Challenge #2: <br /> War Card Game</a></td>
+                <td><a href="Labs/Lab_2/777/index.php">Lab #2: <br /> Slot Machine Game </a></td>
+                <td><a href="Homework/Homework_2/index.html">Homework #2: <br /> Vocab Trainer</a></td>
+                <td><a href="Challenges/Challenge_2/Version_2/card_game_v2.php">Challenge #2: <br /> War Card Game</a></td>
                 <td></td>
                 <td>Final Project</td>
             </tr>
             <tr>
-                <td><a href="Lab_3/index.php">Lab #3: <br /> Silver Jack Game </a></td>
-                <td><a href="Homework_3/index.php">Homework #3: <br /> Manga Previewer</a></td>
+                <td><a href="Labs/Lab_3/index.php">Lab #3: <br /> Silver Jack Game </a></td>
+                <td><a href="Homework/Homework_3/index.php">Homework #3: <br /> Manga Previewer</a></td>
             </tr>
             <tr>
-                <td><a href="Lab_4/Slider/index.php">Lab #4: <br /> Image Searcher </a></td>
+                <td><a href="Labs/Lab_4/Slider/index.php">Lab #4: <br /> Image Searcher </a></td>
             </tr>
             <tr>
-                <td><a href="Lab_5/scart/index.php">Lab #5: <br /> Shopping Cart </a></td>
+                <td><a href="Labs/Lab_5/scart/index.php">Lab #5: <br /> Shopping Cart </a></td>
             </tr>
             <tr>
-                <td><a href="Lab_6/index.php">Lab #6: <br /> Ottermart </a></td>
+                <td><a href="Labs/Lab_6/index.php">Lab #6: <br /> Ottermart </a></td>
             </tr>
             <tr>
-                <td><a href="Lab_7/index.php">Lab #7: <br /> Ottermart - Administration </a></td>
+                <td><a href="Labs/Lab_7/index.php">Lab #7: <br /> Ottermart - Administration </a></td>
             </tr>
         </table>
         
